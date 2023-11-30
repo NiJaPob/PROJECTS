548951c495668962659f52edb81251360dbdca9b
NiJaPob <nick95222@gmail.com>
2023-11-30 09:44:49 -0500

    NBA.py

--- NBA.py
+++ NBA.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
 
-querystring = {"id":"236","season":"2020"}
+querystring = {"id":"236", "season":"2022"}
 
 headers = {
 	"X-RapidAPI-Key": "271ad58b4amshb8b64f539e659fbp1de4e1jsn0f595ea95d29",
